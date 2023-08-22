018771c2a00a85c9b7e9851136af289ce919717f
kosuke430 <kosuke.haga.2w@stu.hosei.ac.jp>
2023-08-23 08:27:53 +0900

    パラメータのロードを出来るように修正

--- lightGBM_model.py
+++ lightGBM_model.py
@@ -1,5 +1,6 @@
 import sys
 
+from sklearn.preprocessing import LabelEncoder
 import pickle
 import pandas as pd
 
@@ -11,16 +12,23 @@ $cmd = "python3 " . $pythonPath . " " . implode(" ", $data);
 //dataはarrayで要素だけを送って受け取るか、jsonにしてファイルに書き込んでから、python側で参照して取り込む
 exec($cmd, $out);
 """
-
+#モデルのパラメータをロード
 load_model_lightGBM=pickle.load(open('parametar.sav','rb'))
 
 df_single = pd.DataFrame([])
+
+le = LabelEncoder()
+sexlabels=['女性','男性']
+sexlabels_id=le.fit_transform(sexlabels)
+
 df_single['hour'] = pd.to_datetime(df_single['今の時間帯']).dt.hour
 df_single['性別'] = le.transform(df_single['性別'])
 df_single.drop('今の時間帯', axis=1, inplace=True)
 
 X_single = df_single
 
+
+#モデルから推論
 pred_prob = load_model_lightGBM.predict(X_single, num_iteration=load_model_lightGBM.best_iteration)
 prediction = 1 if pred_prob[0] >= 0.5 else 0
 if prediction == 1:
