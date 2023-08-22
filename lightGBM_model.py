cb667088c0cc386dc52788b6b49533831e43e541
kosuke430 <kosuke.haga.2w@stu.hosei.ac.jp>
2023-08-23 07:52:25 +0900

    php用のスクリプトの作成とパラメータファイルの作成

---
+++ lightGBM_model.py
@@ -0,0 +1,31 @@
+import sys
+
+import pickle
+import pandas as pd
+
+"""
+$pythonPath = *.~. "/lightGBM_model.py";
+$cmd = "python3 " . $pythonPath . " " . implode(" ", $data);
+
+//Pythonのファイルにデータを送信(dataを受取って、outに返す)
+//dataはarrayで要素だけを送って受け取るか、jsonにしてファイルに書き込んでから、python側で参照して取り込む
+exec($cmd, $out);
+"""
+
+load_model_lightGBM=pickle.load(open('parametar.sav','rb'))
+
+df_single = pd.DataFrame([])
+df_single['hour'] = pd.to_datetime(df_single['今の時間帯']).dt.hour
+df_single['性別'] = le.transform(df_single['性別'])
+df_single.drop('今の時間帯', axis=1, inplace=True)
+
+X_single = df_single
+
+pred_prob = load_model_lightGBM.predict(X_single, num_iteration=load_model_lightGBM.best_iteration)
+prediction = 1 if pred_prob[0] >= 0.5 else 0
+if prediction == 1:
+    print("食事を完食する可能性が高いです")
+else:
+    print("完食しない可能性が高いです")
+
+
